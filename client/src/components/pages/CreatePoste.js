af1eca66172b67ee3d2a139f920ae5d7277c459d
Mohamed Musa <mohamed.musa831@gmail.com>
2020-07-19 13:27:15 +0200

    some changes

--- client/src/components/pages/CreatePoste.js
+++ client/src/components/pages/CreatePoste.js
@@ -11,35 +11,36 @@ const CreatePost = () => {
   const [url, setUrl] = useState("");
   const history = useHistory();
 
-  // useEffect(() => {
-  //   if (url) {
-  //     fetch("/posts/create", {
-  //       method: "post",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         "Authorization": "Bearer " + localStorage.getItem("usertoken")
-  //       },
-  //       body: JSON.stringify({
-  //         title,
-  //         body,
-  //         photo: url
-  //       })
-  //     }).then(res => res.json())
-  //       .then(data => {
-  //         if (data.error) {
-  //           M.toast({
-  //             html: data.error,
-  //             classes: "#c62828 red darken-3",
-  //           });
-  //         } else {
-  //           M.toast({
-  //             html: "Post created successfully",
-  //             classes: "#2e7d32 green darken-3",
-  //           });
-  //         }
-  //       })
-  //   }
-  // }, [url])
+  useEffect(() => {
+    if (url) {
+      fetch("/posts/create", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + localStorage.getItem("usertoken")
+        },
+        body: JSON.stringify({
+          title,
+          body,
+          photo: url
+        })
+      }).then(res => res.json())
+        .then(data => {
+          if (data.error) {
+            M.toast({
+              html: data.error,
+              classes: "#c62828 red darken-3",
+            });
+          } else {
+            M.toast({
+              html: "Post created successfully",
+              classes: "#2e7d32 green darken-3",
+            });
+            history.push('/')
+          }
+        })
+    }
+  }, [url])
 
 
   const addPost = () => {
@@ -57,31 +58,32 @@ const CreatePost = () => {
       })
       .catch(err => console.log(err))
       
-    fetch("/posts/create", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + localStorage.getItem("usertoken")
-      },
-      body: JSON.stringify({
-        title,
-        body,
-        photo: url
-      })
-    }).then(res => res.json())
-      .then(data => {
-        if (data.error) {
-          M.toast({
-            html: data.error,
-            classes: "#c62828 red darken-3",
-          });
-        } else {
-          M.toast({
-            html: "Post created successfully",
-            classes: "#2e7d32 green darken-3",
-          });
-        }
-      })
+    // fetch("/posts/create", {
+    //   method: "post",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     "Authorization": "Bearer " + localStorage.getItem("usertoken")
+    //   },
+    //   body: JSON.stringify({
+    //     title,
+    //     body,
+    //     photo: url
+    //   })
+    // }).then(res => res.json())
+    //   .then(data => {
+    //     if (data.error) {
+    //       M.toast({
+    //         html: data.error,
+    //         classes: "#c62828 red darken-3",
+    //       });
+    //     } else {
+    //       M.toast({
+    //         html: "Post created successfully",
+    //         classes: "#2e7d32 green darken-3",
+    //       });
+    //       history.push('/')
+    //     }
+    //   })
 
 
 
