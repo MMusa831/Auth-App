8e3c5a6f0f7b87dc04f98d492415f0e9cafe6506
Mohamed Musa <mohamed.musa831@gmail.com>
2020-07-19 19:28:16 +0200

    some changes

--- client/src/components/pages/Confirm.js
+++ client/src/components/pages/Confirm.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Axios from 'axios'
+import { useParams } from 'react-router-dom';
 
 function Confirm() {
+    const ConfirmEmail = () => {
+        const token = useParams()
+       Axios.get(`/activate/${token}`)
+       .then(res => {
+
+           console.log(res.token)
+       })
+    }
     return (
         <div>
             <h1>Confirmation Page</h1>
+            <button onClick={ConfirmEmail}>Click</button>
         </div>
     )
 }
