57505cd1fa40b1be35b025be8aa6324cec072a65
Mohamed Musa <mohamed.musa831@gmail.com>
2020-07-20 22:52:32 +0200

    some changes

--- server.js
+++ server.js
@@ -10,11 +10,7 @@ const path = require("path");
 
 const app = express();
 app.use(cors());
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  })
-);
+app.use(express.json({ extended: false }));
 
 // Database Connection
 const db = process.env.MONGO_DB_URI || process.env.MONGODB_URI;
@@ -29,25 +25,17 @@ mongoose
   .catch((err) => console.log(err));
 
 if (process.env.NODE_ENV === "production") {
-  const root = require('path').join(__dirname, 'client', 'build')
-  app.use(express.static(root));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile('index.html', { root });
-  })
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
 // Set up routes
 
 require("./models/postModel");
 require("./models/userModel");
 
-app.use(bodyParser.json());
+
 
 app.use("/users", require("./routes/userRoute"));
 app.use("/posts", require("./routes/postRoute"));
