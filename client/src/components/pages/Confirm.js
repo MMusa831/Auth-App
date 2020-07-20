c591c74f24cf3592adc0d44d7b1e96fb4cc738c4
Mohamed Musa <mohamed.musa831@gmail.com>
2020-07-20 14:44:32 +0200

    some changes

--- client/src/components/pages/Confirm.js
+++ client/src/components/pages/Confirm.js
@@ -1,42 +1,41 @@
-import React,{useEffect} from 'react'
-import Axios from 'axios'
+import React, { useEffect } from "react";
+import Axios from "axios";
 import M from "materialize-css";
 import { notify } from "react-notify-toast";
-import { useParams, useHistory } from 'react-router-dom';
+import { useParams, useHistory } from "react-router-dom";
 
 function Confirm() {
-    const history = useHistory()
-    const { token } = useParams(); 
+  const history = useHistory();
+  const { token } = useParams();
 
-    useEffect(() => {
-        getResponse();
+  useEffect(() => {
+    getResponse();
     //    fetch(`/users/activate/${token}`
-    
+
     // ).then((res) => res.json())
     //     .then((data) => {
     //         console.log(data)
     //       if (data.error) {
-    //           console.log(data.error);           
+    //           console.log(data.error);
     //       } else {
-    //           console.log(data.message);           
+    //           console.log(data.message);
     //         history.push("/login");
     //       }
     //     })
     //     .catch((err) => console.log(err))
-    }, []);
-    const getResponse = async () => {
-      const response = await fetch(`/users/test`);
-       console.log(response);
-      const data = await response.json();
-      console.log(data)
-    };
-        
-    return (
-        <div>
-            <h5 className="text-center">Confirmation Page</h5>
-          
-        </div>
-    )
+  }, []);
+  const getResponse = async () => {
+    const response = await fetch(`/users/test`);
+    console.log(response);
+    const data = await JSON.stringify(response);
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h5 className="text-center">Confirmation Page</h5>
+    </div>
+  );
 }
 
-export default Confirm
+export default Confirm;
