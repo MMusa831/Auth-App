44dcf7d03425af4f788d368d2bce85e00275dc77
Mohamed Musa <mohamed.musa831@gmail.com>
2020-07-20 21:35:28 +0200

    some changes

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ mongoose
   })
   .then(() => console.log("Database is connected!"))
   .catch((err) => console.log(err));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
   //app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
